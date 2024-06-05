b24a8b7674e6f3550c3f8ff21fe589311c1c8abb
bkness <kbrandon863@gmail.com>
2024-06-05 00:22:17 -0700

    Refine project layout with Bootstrap grid and add CSS styling

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Project from '../components/Project';
 import { Container, Row, Col } from 'react-bootstrap';
+// import './Project.css';
 
 const projects = [
   {
@@ -42,10 +43,10 @@ const projects = [
 
 function Portfolio() {
   return (
-    <Container>
+    <Container className="my-5">
       <Row>
         {projects.map((project, index) => (
-          <Col sm={12} md={6} key={index}>
+          <Col sm={12} md={6} lg={6} key={index} className="mb-4">
             <Project project={project} />
           </Col>
         ))}
