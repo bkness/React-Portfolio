5c1c9ec73887da6d7aaf99685dd536a54d810a15
bkness <kbrandon863@gmail.com>
2024-06-04 20:17:45 -0700

    used bootstrap grid to display related work experiences so they appear more visually appealing

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,54 +1,56 @@
-import Project from "../components/Project";
+import React from 'react';
+import Project from '../components/Project';
+import { Container, Row, Col } from 'react-bootstrap';
 
+const projects = [
+  {
+    name: "Tech Blog",
+    description: "Node/JS/CSS/Handlebars",
+    link: "https://mvc--tech-blog-715ec89956fb.herokuapp.com/",
+    repo: "https://github.com/bkness/MVC-Tech-Blog",
+    image: "blog",
+  },
+  {
+    name: "Random Cocktail Generator",
+    description: "HTML/JS/CSS ",
+    link: "https://bkness.github.io/Random-Cocktail-API",
+    repo: "https://github.com/bkness/Random-Cocktail-API",
+    image: "cocktail",
+  },
+  {
+    name: "Open Weather Database",
+    description: "HTML/CSS/JS/OpenWeatherAPI",
+    link: "https://bkness.github.io/open-weather-api",
+    repo: "https://github.com/bkness/open-weather-api",
+    image: "weather",
+  },
+  {
+    name: "Video Game Hub",
+    description: "React/Express/Graphql/Mongo/Redux",
+    link: "https://video-game-hub.onrender.com/",
+    repo: "https://github.com/bkness/video-game-hub-project3",
+    image: "game",
+  },
+  {
+    name: "Key Generator",
+    description: "HTML/CSS/JS",
+    link: "https://bkness.github.io/javascript-key-generator",
+    repo: "https://github.com/bkness/javascript-key-generator",
+    image: "keygen",
+  }
+];
 
 function Portfolio() {
-  // fill in the required data, image file should be in the assets/projects folder and name should match the "default as " statment in index.js
-  const projects = [
-    {
-      name: "Tech Blog",
-      description: "Node/JS/CSS/Handlebars",
-      link: "https://mvc--tech-blog-715ec89956fb.herokuapp.com/",
-      repo: "https://github.com/bkness/MVC-Tech-Blog",
-      image: "blog",
-    },
-    {
-      name: "Random Cocktail Generator",
-      description: "HTML/JS/CSS ",
-      link: "https://bkness.github.io/Random-Cocktail-API",
-      repo: "https://github.com/bkness/Random-Cocktail-API",
-      image: "cocktail",
-    },
-    {
-      name: "Open Weather Database",
-      description: "HTML/CSS/JS/OpenWeatherAPI",
-      link: "https://bkness.github.io/open-weather-api",
-      repo: "https://github.com/bkness/open-weather-api",
-      image: "weather",
-    },
-    {
-      name: "Video Game Hub",
-      description: "React/Express/Graphql/Mongo/Redux",
-      link: "https://video-game-hub.onrender.com/",
-      repo: "https://github.com/bkness/video-game-hub-project3",
-      image: "game",
-    },
-    {
-      name: "Key Generator",
-      description: "HTML/CSS/JS",
-      link: "https://bkness.github.io/javascript-key-generator",
-      repo: "https://github.com/bkness/javascript-key-generator",
-      image: "keygen",
-    }
-  ]
-  // for each project, use the Project component to build a project
   return (
-    <div>
-      <div className="projects">
-        {projects.map((project) => (
-          <Project project={project} key={"project-" + project.name} />
-       ))}
-      </div>
-    </div>
+    <Container>
+      <Row>
+        {projects.map((project, index) => (
+          <Col sm={12} md={6} key={index}>
+            <Project project={project} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
