53549fad514b0d58934816126dc03fd577abcba9
bkness <kbrandon863@gmail.com>
2024-06-05 21:02:03 -0700

    feat: Revise About page content and update CSS for improved professionalism
    - Updated About component content to sound more professional and eager
    - Enhanced CSS with new color scheme variables
    - Styled About container with background color, border radius, and box shadow
    - Adjusted title and text colors for better contrast and readability
    - Applied animation for a polished and engaging user experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 // Bringing in the required import from 'react-router-dom'
+import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Nav from './components/NavTabs';
+import NavTabs from './components/NavTabs';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
-  // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
-      <Nav />
-      <main className="mx-3">
-      
+      <NavTabs />
+      <main className="main">
         <Outlet />
       </main>
       <Footer />
