c5cd3e9cb986246121655154474737d1066996e0
bkness <kbrandon863@gmail.com>
2024-06-07 08:44:32 -0700

    reconfigured contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,30 +2,34 @@ import React from 'react';
 import './Contact.css';
 
 export default function Contact() {
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+  };
+
   return (
     <div className="contact-container">
       <h1>Contact Me</h1>
-      <form name="contact" method="POST" data-netlify="true" enctype="multipart/form-data">
-        <input type="hidden" name="form-name" value="contact" />
-        <div className="contact-input">
-          <input type="text" name="name" id="name" placeholder="Name" />
+      <form name='contact v1' method='POST' data-netlify='true' onSubmit={handleSubmit}>
+        <input type="hidden" name='form-name' value='contact v1' />
+        <div>
+          <label>First Name
+            <input type="text" name="first-name" />
+          </label>
         </div>
-        <div className="contact-input">
-          <input type="email" name="email" id="email" placeholder="Email" />
+        <div>
+          <label htmlFor='email'>Email</label> <br />
+          <input id='email' type="email" name="email" />
         </div>
-        <div className="field">
-          <textarea name="message" id="message" placeholder="Message" rows="7"></textarea>
+        <div>
+          <label>Message
+            <textarea name="message" rows='7'></textarea>
+          </label>
         </div>
-        <div className="field">
-          <input type='file' name='myfile' id='myfile' placeholder='Upload File' rows='7' />
-        </div>
-        <div className="field">
-          <div data-netlify-recaptcha="true"></div>
-        </div>
-        <ul className="actions">
-          <li><input type="submit" value="Send Message" /></li>
-        </ul>
+        <button type='submit'>Submit The Results</button>
       </form>
     </div>
   );
 }
+
+
+
