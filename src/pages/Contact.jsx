50d0bb815e1311e895c023d38acb43bc00217bff
bkness <kbrandon863@gmail.com>
2024-06-07 07:11:29 -0700

    Refactor contact form for improved structure, accessibility, and functionality

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,24 +5,26 @@ export default function Contact() {
   return (
     <div className="contact-container">
       <h1>Contact Me</h1>
-      <form name="contact" method="POST" data-netlify="true">
+      <form action="POST" data-netlify="true" >
         <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label>Your Name: </label>
-          <input type="text" name="name" required />
-        </p>
-        <p>
-          <label>Your Email: </label>
-          <input type="email" name="email" required />
-        </p>
-  
-        <p>
-          <label>Message: </label>
-          <textarea name="message" required></textarea>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
+        <div className="contact-input">
+          <input type="text" name="name" id="name" placeholder="Name" />
+        </div>
+        <div className="contact-input">
+          <input type="email" name="email" id="email" placeholder="Email" />
+        </div>
+        <div className="field">
+          <textarea name="message" id="message" placeholder="Message" rows="7"></textarea>
+        </div>
+        <div className="field">
+          <input type='file' name='myfile' id='myfile' placeholder='Upload File' rows='7' />
+        </div>
+        <div className="field">
+          <div data-netlify-recaptcha='true'></div>
+        </div>
+        <ul className="actions">
+          <li><input type="submit" value="Send Message"  /></li>
+        </ul>
       </form>
     </div>
   );
