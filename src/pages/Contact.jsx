6e2588e676662c10f2aca6d735af36537f42b350
bkness <kbrandon863@gmail.com>
2024-06-07 11:17:07 -0700

    test

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import ContactForm from './ContactForm.jsx';
 import './Contact.css';
 
 export default function Contact() {
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+  };
+
   return (
     <div className="contact-container">
       <h1>Contact Me</h1>
-      <ContactForm />
+      <form name='contact v1' method='POST' data-netlify='true' onSubmit={handleSubmit}>
+        <input type="hidden" name='form-name' value='contact v1' />
+        <div>
+          <label>First Name
+            <input type="text" name="first-name" />
+          </label>
+        </div>
+        <div>
+          <label htmlFor='email'>Email</label> <br />
+          <input id='email' type="email" name="email" />
+        </div>
+        <div>
+          <label>Message
+            <textarea name="message" rows='7'></textarea>
+          </label>
+        </div>
+        <button type='submit'>Submit The Results</button>
+      </form>
     </div>
   );
 }
+
+
+
