9ed2f974ab20d865fcbc8748bf371b8c27b1e378
bkness <kbrandon863@gmail.com>
2024-06-07 07:53:04 -0700

    reconfigured contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,28 +2,10 @@ import React from 'react';
 import './Contact.css';
 
 export default function Contact() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => {
-        console.log('Form successfully submitted');
-        // optionally redirect user to a new page here 
-      })
-      .catch((error) => {
-        console.error('Error submitting form:', error);
-      });
-  };
-
   return (
     <div className="contact-container">
       <h1>Contact Me</h1>
-      <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
+      <form name="contact" method="POST" data-netlify="true" enctype="multipart/form-data">
         <input type="hidden" name="form-name" value="contact" />
         <div className="contact-input">
           <input type="text" name="name" id="name" placeholder="Name" />
@@ -35,7 +17,10 @@ export default function Contact() {
           <textarea name="message" id="message" placeholder="Message" rows="7"></textarea>
         </div>
         <div className="field">
-          <div data-netlify-recaptcha='true'></div>
+          <input type='file' name='myfile' id='myfile' placeholder='Upload File' rows='7' />
+        </div>
+        <div className="field">
+          <div data-netlify-recaptcha="true"></div>
         </div>
         <ul className="actions">
           <li><input type="submit" value="Send Message" /></li>
